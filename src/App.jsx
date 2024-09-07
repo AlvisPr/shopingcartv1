e6e66f41571167e22276708ab41d13414fa7084f
Alvis Prieditis <reverbtail@gmail.com>
2024-09-06 20:13:19 -0500

    halfway implemented

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Products from './Components/products'
+
+import './App.css'
+
+function App() {
+  // use the useState hook if needed
+  // const [state, setState] = useState(initialState);
+
+  return (
+    <>
+      <Products />
+    </>
+  )
+}
+
+export default App
